7bc3263570f77f2403a9b135e83f14027344c6b7
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-09-30 19:18:08 +0300

    added lab 1 task 7

---
+++ lab1/task7.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+type student struct {
+	name  *string
+	kurs  int
+	rating float32
+}
+
+func task7() {
+	var n int
+	fmt.Scan(&n)
+
+	students := make([]student, n)
+
+	for i := 0; i < n; i++ {
+		var name string
+		var kurs int
+		var rating float32
+		fmt.Scan(&name, &kurs, &rating)
+		students[i].name = &name
+		students[i].kurs = kurs
+		students[i].rating = rating
+	}
+
+	for _, s := range students {
+		if s.kurs == 3 {
+			fmt.Printf("Name: %s, Rating: %.2f\n", *s.name, s.rating)
+		}
+	}
+}

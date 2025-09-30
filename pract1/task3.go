a56329985b71460ee9b1fa2fa277851200ee4573
NureKozhanovOleksandr <oleksandr.kozhanov@nure.ua>
2025-09-30 16:44:25 +0300

    added pw 1 task 3

---
+++ pract1/task3.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var element int
+	var value float64
+	fmt.Scan(&element, &value)
+
+	if value <= 0 {
+		fmt.Println("Error: Value must be positive")
+		return
+	}
+
+	var a, b, h, S float64
+
+	switch element {
+	case 1:
+		a = value
+		b = a * math.Sqrt(2)
+		h = a / math.Sqrt(2)
+		S = (a * a) / 2
+
+	case 2:
+		b = value
+		a = b / math.Sqrt(2)
+		h = a / math.Sqrt(2)
+		S = (a * a) / 2
+
+	case 3:
+		h = value
+		a = h * math.Sqrt(2)
+		b = a * math.Sqrt(2)
+		S = (a * a) / 2
+		
+	case 4:
+		S = value
+		a = math.Sqrt(2 * S)
+		b = a * math.Sqrt(2)
+		h = a / math.Sqrt(2)
+
+	default:
+		fmt.Println("Error: Invalid element number")
+		return
+	}
+
+	fmt.Printf("Cathetus: %.2f\n", a)
+	fmt.Printf("Hypotenuse: %.2f\n", b)
+	fmt.Printf("Height: %.2f\n", h)
+	fmt.Printf("Area: %.2f\n", S)
+}

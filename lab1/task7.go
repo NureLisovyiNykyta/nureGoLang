21bacce502bb0814c1b9430c1ae49c773146a321
Nikita Lisovyi <nykyta.lisovyi@nure.ua>
2025-10-17 21:05:15 +0300

    fix pz1 task7

--- lab1/task7.go
+++ lab1/task7.go
@@ -21,7 +21,7 @@ func task7() {
 		var name string
 		var kurs int
 		var rating float32
-		fmt.Print("Введіть ім'я, курс та рейтинг студента: ")
+		fmt.Print("Введіть ім'я, курс та рейтинг (1-5) студента: ")
 		fmt.Scan(&name, &kurs, &rating)
 		students[i].name = &name
 		students[i].kurs = kurs
@@ -29,8 +29,25 @@ func task7() {
 	}
 
 	for _, s := range students {
-		if s.kurs == 3 {
+		if s.rating < 3 {
 			fmt.Printf("Ім'я: %s, Рейтинг: %.2f\n", *s.name, s.rating)
 		}
 	}
+
+	fmt.Println("Введіть кількість студентів для видалення з кінця списку:")
+	var count int
+	fmt.Scan(&count)
+
+	if count > n {
+		count = n
+	}
+
+	new_students := make([]student, n-count)
+
+	fmt.Println("Оновлений список студентів:")
+	for i := 0; i < n-count; i++ {
+		new_students[i] = students[i]
+		fmt.Printf("Ім'я: %s, Курс: %d, Рейтинг: %.2f\n", *new_students[i].name, new_students[i].kurs, new_students[i].rating)
+	}
+	students = new_students
 }
